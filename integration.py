3b5f87e57589b297cbc9417d2f61d4d9eb732f71
AgustinPardo <agu_pardo@hotmail.com>
2019-05-07 11:05:53 +0100

    Uniprot id of uncurated genes column added

--- integration.py
+++ integration.py
@@ -10,7 +10,15 @@ salida = open('uncurated_proteins_found_in_biocyc.tsv','w')
 # Creo una lista de los genes no curados(puede haber mas de uno por fila del archivo)
 uncurated_file			=		pd.read_csv('/home/agustin/Downloads/Biocyc_integration/files/uncurated_proteins.csv')
 uncurated_file 			= 		uncurated_file[uncurated_file['Actual release'].isnull()]
-genes_uncurated_list 	= 		uncurated_file['Gene name'].unique()
+# ~ genes_uncurated_list 	= 		uncurated_file['Gene name']
+# ~ uniprot_uncurated_list 	= 		uncurated_file['Entry']
+
+a=uncurated_file[['Gene name','Entry']]
+genes_uniprot_list=[]
+for r in a.itertuples():
+	genes=[r[1]]
+	uniprot=[r[2]]
+	genes_uniprot_list.append([genes,uniprot])
 
 with open('/home/agustin/Downloads/Biocyc_integration/files/HS_genes.json') as json.file:
 	data = json.load(json.file)
@@ -19,39 +27,39 @@ with open('/home/agustin/Downloads/Biocyc_integration/files/HS_genes.json') as j
 
 biocyc_genes_dic = {}
 
-# Recorro el json. Cada row es un gen.
+# Recorro el json Cada row es un gen.
 # Creo lista de listas con los genes sinonimos de Biocyc
-for row in data['rows']:
-		
+for row in data['rows']:		
 	gene_names			=		row[3]	
 	gene_names_aux		=		row[4]		
 	pathways_names		=		row[5]	
 	reactions_names		=		row[6]
-
 	if pathways_names != '' or reactions_names != '':		
 		parser.extract_content(gene_names,gene_names_aux,pathways_names,reactions_names,biocyc_genes_dic)
 				
-# Comparo la lista de genes no curados y el diccionario de biocyc
-
+# Abro archivos
 reaction_list = [line.rstrip('\n') for line in open('reaction_list.txt')]
 evidence_list = [line.rstrip('\n') for line in open('reaction_evidence_codes.txt')]
-
 with open('reaction_evidence_codes_dict.json', 'r') as f:
 	distros_dict = json.load(f)
 	dict_reaction_evidence_code = dict((k.encode('utf8'), v.encode('utf8')) for k, v in distros_dict.items())
 
-string_evidence = ('\t').join(evidence_list)
-
 # Creo el header del archivo de salida
-salida.write('Uncurated-gene-id'+'\t'+'Uncurated-gene-synonims'+'\t'+'Biocyc-gene-sinomys'+\
+string_evidence = ('\t').join(evidence_list)
+salida.write('Uncurated-gene-id'+'\t'+'Uncurated-gene-uniprot-id'+'\t'+'Uncurated-gene-synonims'+'\t'+'Biocyc-gene-sinomys'+\
 '\t'+'Pathway-Biocyc-label'+'\t'+'Pathway-Biocyc-id'+'\t'+'Reaction-Biocyc-label'+'\t'\
 +'Reaction-Biocyc-id'+'\t'+string_evidence+"\n")	
 
 evidence_refPos_dic=biocyc_retrieve.dict_evidence_refPos(evidence_list)
 
-for genes_uncurated in genes_uncurated_list:
-	
-	genes_uncurated		=		genes_uncurated.split(' ')	
+# Comparo la lista de genes no curados y el diccionario de biocyc
+for genes_uniprot_uncurated in genes_uniprot_list:
+	print(genes_uniprot_uncurated)
+	print(genes_uniprot_uncurated[0])
+	print(genes_uniprot_uncurated[1])
+
+	genes_uncurated		=		genes_uniprot_uncurated[0][0].split(' ')	
+	uniprot_id			=		genes_uniprot_uncurated[1][0]	
 	found				=		False
 	i=0
 	
@@ -75,14 +83,12 @@ for genes_uncurated in genes_uncurated_list:
 				
 				
 				# Busco el Evidence code de cada reaccion				
-				string_evidences = biocyc_retrieve.string_evidence_code(found_reacts[1], dict_reaction_evidence_code, evidence_list, evidence_refPos_dic)
-								
-				#				
+				string_evidences = biocyc_retrieve.string_evidence_code(found_reacts[1], dict_reaction_evidence_code, evidence_list, evidence_refPos_dic)			
 
 				found_gene_sinomys=found[-1]
 				
 				# Escribo en la salida el resultado de la iteracion	
-				salida.write(genes_uncurated[0]+'\t'+str(genes_uncurated)\
+				salida.write(genes_uncurated[0]+'\t'+uniprot_id+'\t'+str(genes_uncurated)\
 				+'\t'+str(found_gene_sinomys)+'\t'+path_label+'\t'+path_id\
 				+'\t'+react_label+'\t'+react_id+'\t'+string_evidences+"\n")			
 			else:
